06dc079f88367261018655f3f0b3a52abb1bcca8
Unknown <zen.lednik7@gmail.com>
2017-04-29 17:42:50 +0200

    Added basic Emojidb inserts

--- app/src/main/java/com/kukumoraketo/emojibrowser/BrowserActivity.java
+++ app/src/main/java/com/kukumoraketo/emojibrowser/BrowserActivity.java
@@ -3,11 +3,16 @@ package com.kukumoraketo.emojibrowser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.kukumoraketo.emojibrowser.Emoji.EmojiDb.EmojiDb;
+
 public class BrowserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browser);
+
+        EmojiDb db = new EmojiDb(getApplicationContext());
+
     }
 }
