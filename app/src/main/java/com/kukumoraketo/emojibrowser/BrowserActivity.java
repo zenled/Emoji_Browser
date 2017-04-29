17a11e101e7fe8808d0ad50c34e7c5e21e07dc6d
Unknown <zen.lednik7@gmail.com>
2017-04-29 14:25:30 +0200

    Added basic Android project

---
+++ app/src/main/java/com/kukumoraketo/emojibrowser/BrowserActivity.java
@@ -0,0 +1,13 @@
+package com.kukumoraketo.emojibrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BrowserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browser);
+    }
+}
