c4ef315a160188ca7e5582bd9718afc1f4c9e484
Unknown <zen.lednik7@gmail.com>
2017-05-10 16:01:07 +0200

    Fixed wrong column in SELECT statement

--- Emoji_Metadata/SQL/select/select_code_by_keywords/pt_1.sql
+++ Emoji_Metadata/SQL/select/select_code_by_keywords/pt_1.sql
@@ -1,4 +1,4 @@
-SELECT Emoji.id
+SELECT Emoji.code
 FROM Emoji JOIN Emoji_Keyword ON (Emoji.id = Emoji_Keyword.emoji_id)
 		JOIN Keyword ON (Emoji_Keyword.keyword_id = Keyword.id)
 WHERE Keyword.keyword LIKE "
