9a9175f2a4c92abff4c470981c744c8d13cb882d
Unknown <zen.lednik7@gmail.com>
2017-05-13 16:40:14 +0200

    Added text to AboutDialogFragment

--- app/src/main/java/com/kukumoraketo/emojibrowser/AboutDialogFragment.java
+++ app/src/main/java/com/kukumoraketo/emojibrowser/AboutDialogFragment.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by zed on 9.5.2017.
@@ -36,6 +39,16 @@ public class AboutDialogFragment extends DialogFragment {
                 dialog.dismiss();
             }
         });
+
+        //region sets text
+        TextView aboutContent = (TextView) mainView.findViewById(R.id.about_content_TextView);
+        aboutContent.setText(Html.fromHtml(getString(R.string.about_content)));
+        aboutContent.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView licenseContent = (TextView) mainView.findViewById(R.id.about_license_content_TextView);
+        licenseContent.setText(Html.fromHtml(getString(R.string.about_license_content)));
+        licenseContent.setMovementMethod(LinkMovementMethod.getInstance());
+        //endregion
 
         return builder.create();
     }
