debf28f1f08015b3a51c0975b45df5813636f407
Unknown <zen.lednik7@gmail.com>
2017-05-14 13:26:13 +0200

    Updated SQL statements inside app

--- app/src/main/java/com/kukumoraketo/emojibrowser/Emoji/EmojiDb/EmojiDbHelper.java
+++ app/src/main/java/com/kukumoraketo/emojibrowser/Emoji/EmojiDb/EmojiDbHelper.java
@@ -22,7 +22,7 @@ public class EmojiDbHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         // Creates Database
         db.execSQL("CREATE TABLE `Category` ( `id` INTEGER NOT NULL UNIQUE, `category` TEXT NOT NULL, PRIMARY KEY(`id`) )");
-        db.execSQL("CREATE TABLE \"Emoji\" ( `id` INTEGER NOT NULL UNIQUE, `code` TEXT NOT NULL UNIQUE, `name` TEXT NOT NULL, `short_name` TEXT, `has_tone` INTEGER NOT NULL, `tone` INTEGER, `emoji_order` INTEGER NOT NULL UNIQUE, `category_id` INTEGER NOT NULL, PRIMARY KEY(`id`) )");
+        db.execSQL("CREATE TABLE \"Emoji\" ( `id` INTEGER NOT NULL UNIQUE, `code` TEXT NOT NULL UNIQUE, `code_point` TEXT NOT NULL UNIQUE, `name` TEXT NOT NULL, `short_name` TEXT, `has_tone` INTEGER NOT NULL, `tone` INTEGER, `emoji_order` INTEGER NOT NULL UNIQUE, `category_id` INTEGER NOT NULL, PRIMARY KEY(id) )");
         db.execSQL("CREATE TABLE `Keyword` ( `id` INTEGER NOT NULL UNIQUE, `keyword` TEXT NOT NULL, PRIMARY KEY(`id`) )");
         db.execSQL("CREATE TABLE `Emoji_Keyword` ( `emoji_id` INTEGER NOT NULL, `keyword_id` INTEGER NOT NULL, PRIMARY KEY(`emoji_id`,`keyword_id`) )");
 
