4e6a06105fc0f8daf79aca6ceec509034ec09cae
Unknown <zen.lednik7@gmail.com>
2017-05-15 19:07:27 +0200

    Added ability to share emoji (via text)

--- app/src/main/java/com/kukumoraketo/emojibrowser/EmojiDisplay/EmojiDetailsDialogFragment.java
+++ app/src/main/java/com/kukumoraketo/emojibrowser/EmojiDisplay/EmojiDetailsDialogFragment.java
@@ -5,6 +5,7 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -117,6 +118,20 @@ public class EmojiDetailsDialogFragment extends DialogFragment {
 
                 Snackbar snackbar = Snackbar.make(v,"Copied to ClipBoard", Snackbar.LENGTH_LONG);
                 snackbar.show();
+            }
+        });
+        //endregion
+
+        //region setsShareButton
+        Button shareButton = (Button) mainView.findViewById(R.id.emoji_share_buton);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, emoji.getUnicodeProper());
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
             }
         });
         //endregion
